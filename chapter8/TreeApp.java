a118b1f39b57f8b23a8141e250807ea768ad1b10
Almas M <kramal888@gmail.com>
2019-06-16 20:08:03 +0600

    binary tree insert deletion and display added

--- chapter8/TreeApp.java
+++ chapter8/TreeApp.java
@@ -1,3 +1,6 @@
+import java.util.Stack;
+import java.io.*;
+
 class Node
 {
     int iDate;
@@ -78,6 +81,16 @@ class Tree
         }
     }
 
+    public void preOrder(Node localRoot)
+    {
+        if(localRoot != null)
+        {
+            System.out.print(localRoot.iDate + " " );
+            inOrder(localRoot.leftChild);
+            inOrder(localRoot.rightChild);
+        }
+    }
+
     public void inOrder(Node localRoot)
     {
         if(localRoot != null)
@@ -85,6 +98,16 @@ class Tree
             inOrder(localRoot.leftChild);
             System.out.print(localRoot.iDate + " " );
             inOrder(localRoot.rightChild);
+        }
+    }
+
+    public void postOrder(Node localRoot)
+    {
+        if(localRoot != null)
+        {
+            inOrder(localRoot.leftChild);
+            inOrder(localRoot.rightChild);
+            System.out.print(localRoot.iDate + " " );
         }
     }
 
@@ -233,6 +256,72 @@ class Tree
 
         return hasOne;
     }
+
+    public void traverse(int traverseType)
+    {
+        switch(traverseType)
+        {
+            case 1: System.out.print("\nPreorder traversal: ");
+                preOrder(root);
+                break;
+            case 2: System.out.print("\nInorder traversal: ");
+                inOrder(root);
+                break;
+            case 3: System.out.print("\nPostorder traversal");
+                postOrder(root);
+                break;
+        }
+        System.out.println();
+    }
+
+    public void displayTree()
+    {
+        Stack globalStack = new Stack();
+        globalStack.push(root);
+        int nBlanks = 32;
+        boolean isRowEmpty = false;
+
+        System.out.println("..................");
+
+        while( isRowEmpty == false)
+        {
+            Stack localStack = new Stack();
+            isRowEmpty = true;
+
+            for(int j=0; j<nBlanks; j++)
+                System.out.print(' ');
+            
+            while(globalStack.isEmpty() == false)
+            {
+                Node temp = (Node)globalStack.pop();
+                if(temp != null)
+                {
+                    System.out.print(temp.iDate);
+                    localStack.push(temp.leftChild);
+                    localStack.push(temp.rightChild);
+
+                    if(temp.leftChild != null || temp.rightChild != null)
+                        isRowEmpty = false;
+                }
+                else
+                {
+                    System.out.print("--");
+                    localStack.push(null);
+                    localStack.push(null);
+                }
+
+                for(int j=0; j<nBlanks*2-2; j++)
+                {
+                    System.out.print(' ');
+                }
+            }
+            System.out.println();
+            nBlanks /= 2;
+            while(localStack.isEmpty() == false)
+                globalStack.push(localStack.pop());
+        }
+        System.out.println(".............................");
+    }
 }
 
 class TreeApp
@@ -262,6 +351,8 @@ class TreeApp
         System.out.println("Now we begin to delete nodes");
         theTree.delete(90);
         theTree.inOrder(theTree.getRoot());
+
+        theTree.displayTree();
 
         
 
